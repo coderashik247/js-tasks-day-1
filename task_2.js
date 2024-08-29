7449beee762a646a93988d630aa975fee1b1356e
Ashik <ashikurashik20999@gmail.com>
2024-08-29 15:56:20 +0600

    task_2 added

---
+++ task_2.js
@@ -0,0 +1,9 @@
+var mathMark = 75.25;
+var biologyMark = 65;
+var chemistryMark = 80;
+var physicsMark = 35.45;
+var banglaMark = 99.50;
+
+var avgOfMark = (mathMark + biologyMark + chemistryMark + physicsMark +banglaMark)/5;
+
+console.log(avgOfMark.toFixed(2));
